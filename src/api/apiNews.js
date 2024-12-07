cf0ee75bcfc0f6b6499f47113804a008975e289d
Dzhonni_D <31665262+dzhekanator1993@users.noreply.github.com>
2024-12-07 09:18:33 +0000

    env

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-// const API_KEY = import.meta.env.VITE_NEWS_API_KEY
-const API_KEY =`aPLdDfQrL3IoKUlwq1u5FJEmGwCBHOKKJpQ0Dst9wXbLdW6t`
-// const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
-const BASE_URL =`https://api.currentsapi.services/v1/`
+const API_KEY = import.meta.env.VITE_NEWS_API_KEY
+// const API_KEY =`aPLdDfQrL3IoKUlwq1u5FJEmGwCBHOKKJpQ0Dst9wXbLdW6t`
+const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
+// const BASE_URL =`https://api.currentsapi.services/v1/`
 
 
 export const getNews = async () => {
