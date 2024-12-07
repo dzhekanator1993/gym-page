1c9b99e3f860cd672d4ab172980985b571e6a5cb
Dzhonni_D <31665262+dzhekanator1993@users.noreply.github.com>
2024-12-07 13:46:23 +0000

    react to vite

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -1,8 +1,10 @@
-import axios from "axios"
+import axios from "axios";
+
 
 console.log('import.meta.env:', import.meta.env);
 
-const API_KEY = import.meta.env.VITE_NEWS_API_KEY
+const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+console.log(API_KEY);
 
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
 
