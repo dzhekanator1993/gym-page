668046e6b46a432d20f9984a8bcccd00f3ebe268
Dzhonni_D <31665262+dzhekanator1993@users.noreply.github.com>
2024-12-08 22:42:49 +0000

    відловлювання помилок

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -11,6 +11,7 @@ const News = () => {
     const [news, setNews] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [currentPage, setCurrentPage] = useState(1)
+    const [error, setError] = useState(null); // Стан для помилок
     const totalPages = 10
     const pageSize = 10
 
@@ -25,6 +26,29 @@ const News = () => {
         }
     }
     useEffect(() => {
+        
+        fetch('/api/news')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP помилка: ${response.status}`);
+            }
+            return response.json(); // Перетворення відповіді на JSON
+        })
+        .then(data => {
+            console.log('Отримані дані:', data); //Перевіряємо структуру відповіді
+            setNews(data || []); // Забезпечуємо, що новини завжди будуть масивом
+            setNews(data.news); // Встановлюємо дані в стан
+            setIsLoading(false); // Завершили завантаження
+            setError(null); // Очищаємо помилки
+        })
+        .catch(error => {
+            console.error('Помилка при завантаженні новин:', error);
+            setError('Не вдалося завантажити новини.'); // Встановлюємо повідомлення про помилку
+            setNews([]); // Запобігаємо undefined
+            setIsLoading(false); // Завершили завантаження
+            
+        });
+
         fetchNews(currentPage);
     }, [currentPage])
 
@@ -42,6 +66,17 @@ const News = () => {
         setCurrentPage(pageNumber)
     }
 
+    if (isLoading) {
+        return <p>Завантаження новин...</p>;
+    }
+    if (!news.length) {
+        return <p>Новини відсутні.</p>;
+    }
+    if (error) {
+        return <p>{error}</p>; // Показуємо повідомлення про помилку
+    }
+    
+
     return (
         <>
             <div className={styles.container}>
@@ -49,6 +84,7 @@ const News = () => {
                 <main className={styles.main}>
 
                     {news.length > 0 && !isLoading ? <Banner item={news[0]} /> : <Skeleton type={'banner'} count={1} />}
+                    
                     <Pagination
                         handlePreviousPage={handlePreviousPage}
                         handlePageClick={handlePageClick}
