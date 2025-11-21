0143bb4b4e114992e0fe9c7cc396a957db412f36
dzhekanator1993 <dzhekanator1993@gmail.com>
2025-11-22 01:11:01 +0200

    fix: add eslint-disable for useEffect dependency

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -59,6 +59,7 @@ export default function Home() {
 
     useEffect(() => {
         fetchNews();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const fetchNews = async () => {
